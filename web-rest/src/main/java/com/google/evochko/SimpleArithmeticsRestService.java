77609cc2813ba1a6dad9d697ae70b9d91eb423ad
Sergey Evochko <sevochko@integrity.com.ua>
2017-03-11 23:58:00 +0200

    11

--- web-rest/src/main/java/com/google/evochko/SimpleArithmeticsRestService.java
+++ web-rest/src/main/java/com/google/evochko/SimpleArithmeticsRestService.java
@@ -1,29 +1,27 @@
 package com.google.evochko;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.InputStream;
 
-
-@Path("/SimpleArithmetic")
+@ApplicationPath("/SimpleArithmetic")
+@Path("/api")
 public class SimpleArithmeticsRestService {
     @GET
-    @Path("/api/verify")
+    @Path("/verify")
     @Produces(MediaType.TEXT_PLAIN)
     public Response verifyRestService(InputStream inputStream) {
-        String result = "SimpleArithmeticsRestService successfully started..";
+        String result = "SimpleArithmeticsRestService successfully1 started..";
         return Response.status(200).entity(result).build();
     }
 
     @GET
-    @Path("/api/calc/{x1}/{x2}")
+    @Path("/calc/{x1}/{x2}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response calculate(@PathParam("x1") String x1, @PathParam("x2") String x2) {
-        String result = "{'x1':" + x1 + ",'x2':" + x2 + "}\n";
+        System.out.println("x1 = " + x1);
+        String result = "{'x1':" + x1 + ",'x2':" + x2 + "}";
         return Response.status(200).entity(result).build();
     }
 }
