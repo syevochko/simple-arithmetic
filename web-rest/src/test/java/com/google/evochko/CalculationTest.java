56dc7968e203eef3a004af487071d97ecec6d723
Sergey Yevochko <sevochko@integrity.com.ua>
2017-03-14 10:48:48 +0200

    add UI

--- web-rest/src/test/java/com/google/evochko/CalculationTest.java
+++ web-rest/src/test/java/com/google/evochko/CalculationTest.java
@@ -1,28 +1,26 @@
 package com.google.evochko;
 
-import junit.framework.Assert;
 import matchParser.MatchParser;
+import org.junit.Assert;
 
-public class CalculationTest {
+public class CalculationTest extends Assert {
     private static MatchParser calculation = new MatchParser();
 
-//    @Test
-//    public void testMultiply() throws Exception {
-//        Assert.assertEquals(3.75, calculation.Parse("1.5*2.5"));
-//    }
-//
-//    @Test
-//    public void testMultiOperations() throws Exception {
-//        Assert.assertEquals(-36.55, calculation.Parse("(1.5-2.5)*36.55"));
-//    }
-//
-//    @Test
-//    public void testFunctiion() throws Exception {
-//        calculation.setVariable("x1", 1.5);
-//        calculation.setVariable("x2", -1.5);
-//        calculation.setVariable("x3", 5.36);
-//        Assert.assertEquals(0.0, calculation.Parse("(x1+x2)*x3"));
-//
-//        Assert.assertEquals(0.0, calculation.Parse("(x11+x2*x3"));
-//    }
+    @org.junit.Test
+    public void testMultiply() throws Exception {
+        assertEquals(3.75, calculation.Parse("1.5*2.5"), 0);
+    }
+
+    @org.junit.Test
+    public void testMultiOperations() throws Exception {
+        assertEquals(-36.55, calculation.Parse("(1.5-2.5)*36.55"), 0);
+    }
+
+    @org.junit.Test
+    public void testFunctiion() throws Exception {
+        calculation.setVariable("x1", 1.5);
+        calculation.setVariable("x2", -1.5);
+        calculation.setVariable("x3", 5.36);
+        assertEquals(0.0, calculation.Parse("(x1+x2)*x3"), 0);
+    }
 }
